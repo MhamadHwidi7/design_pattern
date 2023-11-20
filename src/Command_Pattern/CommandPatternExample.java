af14fecab3513d69edc863048aaeb0ab263dd074
MhamadHwidi7 <mhamadhwidi440@gmail.com>
2023-11-20 18:27:35 +0300

    first commit command pattern

---
+++ src/Command_Pattern/CommandPatternExample.java
@@ -0,0 +1,86 @@
+package Command_Pattern;
+
+// Command interface
+interface Command {
+    void execute();
+}
+
+// Concrete command classes
+class LightOnCommand implements Command {
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+}
+
+class LightOffCommand implements Command {
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+}
+
+// Receiver class
+class Light {
+    private String location;
+
+    public Light(String location) {
+        this.location = location;
+    }
+
+    public void turnOn() {
+        System.out.println(location + " light is ON");
+    }
+
+    public void turnOff() {
+        System.out.println(location + " light is OFF");
+    }
+}
+
+// Invoker class
+class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
+
+// Client code
+class CommandPatternExample {
+    public static void main(String[] args) {
+        // Create a light and corresponding command objects
+        Light livingRoomLight = new Light("Living Room");
+        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+
+        // Create a remote control and set the command
+        RemoteControl remote = new RemoteControl();
+        remote.setCommand(livingRoomLightOn);
+
+        // Press the button to turn on the light
+        remote.pressButton();
+
+        // Change the command to turn off the light
+        remote.setCommand(livingRoomLightOff);
+
+        // Press the button to turn off the light
+        remote.pressButton();
+    }
+}
